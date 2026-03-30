7bc0e2f82f27d6bb6924ed5b6ee4d79a36084dea
a.s.ushakov <a.s.ushakov@efko.ru>
2026-03-30 23:20:45 +0300

    изменение внешнего вида

--- main.js
+++ main.js
@@ -25,7 +25,7 @@
                 observer.unobserve(entry.target);
             });
         },
-        { threshold: 0.16, rootMargin: '0px 0px -12% 0px' }
+        { threshold: 0.08, rootMargin: '0px 0px 2% 0px' }
     );
 
     items.forEach(item => io.observe(item));
